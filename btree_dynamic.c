c76dbf4436a4fd767d1824c3a198c0125ed6af6e
aryanatatif425-creator <arya.nata.tif425@polban.ac.id>
2026-06-20 09:18:26 +0700

    feat: penambahan utilitas pengurutan dan penamaan barang

--- btree_dynamic.c
+++ btree_dynamic.c
@@ -29,4 +29,39 @@ void InisialisasiTrie(Tree *root) {
     *root = Alokasi(-1); // Nilai default untuk Trie
 }
 
+// Mendapatkan string nama barang berdasarkan angka representasinya
+void AmbilNamaBarang(infotype idBarang, char* namaBarang) {
+    switch(idBarang) {
+        case 1:
+            strcpy(namaBarang, "Rinso");
+            break;
+        case 2:
+            strcpy(namaBarang, "Susu");
+            break;
+        case 3:
+            strcpy(namaBarang, "Beras");
+            break;
+        case 4:
+            strcpy(namaBarang, "Gula");
+            break;
+        default:
+            sprintf(namaBarang, "Item%d", idBarang);
+            break;
+    }
+}
+
+// Modul untuk mengurutkan array
+void UrutkanDaftar(infotype *daftar, int n) {
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (daftar[i] > daftar[j]) {
+                int temp = daftar[i];
+                daftar[i] = daftar[j];
+                daftar[j] = temp;
+            }
+        }
+    }
+}
+
 
